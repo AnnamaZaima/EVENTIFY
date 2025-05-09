66c7407048ff97c6333f59f1e89933ec6d0fc624
Syed Hasibur Rahman Shafin <133050017+HasiburRahmanShafin@users.noreply.github.com>
2025-05-09 22:59:59 +0600

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,9 +1,22 @@
 import mysql.connector
+from flask import g
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='your_password',  # <- replace
-        database='eventify'
-    )
+def init_db(app):
+    def connect_db():
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your_mysql_password",
+            database="eventify"
+        )
+    
+    @app.before_request
+    def before_request():
+        g.db = connect_db()
+        g.cursor = g.db.cursor(dictionary=True)
+
+    @app.teardown_request
+    def teardown_request(exception):
+        db = g.pop('db', None)
+        if db is not None:
+            db.close()
