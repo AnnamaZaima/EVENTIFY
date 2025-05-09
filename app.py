3cdb1b066f955c8e958f8bc4b1bc41e64def876f
Syed Hasibur Rahman Shafin <133050017+HasiburRahmanShafin@users.noreply.github.com>
2025-05-09 22:59:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.auth_routes import auth_bp
-from routes.event_routes import event_bp
-from routes.ticket_routes import ticket_bp
+from backend.routes.auth_routes import auth_bp
+from backend.routes.event_routes import event_bp
+from backend.routes.ticket_routes import ticket_bp
+from backend.db_config import init_db
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    init_db(app)
 
-# Register routes
-app.register_blueprint(auth_bp)
-app.register_blueprint(event_bp)
-app.register_blueprint(ticket_bp)
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(event_bp, url_prefix='/api/events')
+    app.register_blueprint(ticket_bp, url_prefix='/api/tickets')
 
-if __name__ == '__main__':
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
